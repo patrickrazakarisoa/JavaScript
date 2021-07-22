86307d1bbdc093ed4c5dafe610dcd316e5b268b8
kenjy <kenjyzakary@hotmail.com>
2021-07-22 14:54:52 +0200

    8- Les fonctions anonymes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,7 @@
-function prevoirAge(){
+// let fonctionAnonyme = function() {
+//      console.log("Je suis une fonction anonyme.");
+// }
 
-     let age = prompt("Quel âge avez-vous ?"); // stock en chaîne de caractère // "30" + 1 = 301 et non 30+1
-     // age = parseInt(age); // "30" = 30 
-     age = parseFloat(age);
-     // age = Number(age);
+// fonctionAnonyme();
 
-     // nombre = 45;
-     // nombreEnString = nombre + ""; // barbare
-     // nombreEnString = nombre.toString(); // mettre un nombre en chaîne de caractrère déjà implémenter dans JavaScript
-     alert("Bientôt vous aurez " + (age + 1) +  " ans.");
-}
-
-prevoirAge();
+(function() { console.log("Je suis une fonction anonyme.") }) ();
