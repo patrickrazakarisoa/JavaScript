1e30e6dff76f5d2ed0aa0afbffa9137ec75c4536
kenjy <kenjyzakary@hotmail.com>
2021-07-23 14:39:12 +0200

    14 - L'utilisation de ! dans une condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-let gareDeDepart = "Valenciennes";
-let gareDArrivee = "";
-let chauffeur    = "";
+let x = 7;
 
-if ( (gareDeDepart != "" || gareDArrivee != "")  && chauffeur != "") {
-     console.log("Le train va démarrer");
+// ! : permet d'inverser les conditions dans IF de FALSE en TRUE dans ce cas precis.
+// x est différent de 5 mais avec " ! " x est inféreur à 5.
+if( !x < 5) {
+     console.log("x est inférieur à 5.");
 } else {
-     console.log("Le train ne peut pas démarrer.");
+     console.log("x n'est pas inférieur à 5.");
 }
