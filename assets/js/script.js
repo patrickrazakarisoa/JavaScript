2ea70b39e0a6e8b10402d2070473fcdfba3dcd9f
kenjy <kenjyzakary@hotmail.com>
2021-07-24 16:07:35 +0200

    16- Boucle while

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,8 @@
-let gareDeDepart = "Valenciennes";
-let gareDArrivee = prompt("Où souhaitez-vous allez ?") || "Gard du Nord" // Valeur par défaut si le voyageur choisit de ne rien mettre NULL;
-let chauffeur    = "Bob";
+let i = 1;
 
-if ( (gareDeDepart != "" || gareDArrivee != "")  && chauffeur != "") {
-     alert("Le train va démarrer à destination de " + gareDArrivee + ".");
-} else {
-     alert("Le train ne peut pas démarrer.");
+while (i < 5) {
+     
+     console.log("Ligne : " + i);
+     i++ // i = i + 1
 }
 
-// let x = 2;
-
-// Une condition ternaire
-//[CONDITION] ? [TRUE] : [FALSE];
-// x > 3 ? console.log("x est supérieur à 3.") : console.log("x est inférieur à 3") ;
-
-
-// if(x > 3) {
-//      console.log("x est supérieur à 3.");
-// } else {
-//      console.log("x est inférieur à 3");
-// }
