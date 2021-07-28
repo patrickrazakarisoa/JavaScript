eb5b3b9b37c1db0d17025f244025c92c06e4021d
kenjy <kenjyzakary@hotmail.com>
2021-07-28 11:51:41 +0200

    28- Retrouver un élément dans un tableau

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
 
 let monTableau = ['un', 'deux', 'trois', 'quatre']; 
-// monTableau.push('cinq'); 
-// monTableau.unshift('zero') 
-// monTableau.pop(monTableau) 
-// monTableau.shift(monTableau);
-// console.log(monTableau);
+console.log(monTableau.indexOf('trois'));
 
 let monTableau2D = [
      ['Mark', 'Jeff', 'Bill'],
      ['Zuckerberg', 'Bezos', 'Gates']
 ];
-
-// monTableau2D[1].unshift('test');
-// monTableau2D[0].shift();
 // console.log(monTableau2D);
 
 let monTableauAssociatif = { 
@@ -20,7 +13,4 @@ let monTableauAssociatif = {
      'nom'     : 'Zuckerberg',
      'poste'   : 'PDG de Facebook'
 };
-
-// monTableauAssociatif['nationalité'] = 'Américaine';
-delete(monTableauAssociatif.poste);
-console.log(monTableauAssociatif);
+// console.log(monTableauAssociatif);
