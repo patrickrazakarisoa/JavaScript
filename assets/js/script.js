36e53a5e2618fbe9d3ef6b6e99c91a10a768de81
kenjy <kenjyzakary@hotmail.com>
2021-07-28 07:15:28 +0200

    25- Les différents tableaux et comment accéder aux valeurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
-function somme(nombre){
+// Comment créer des tableaux
 
-     if( nombre == 1 ){
-          return 1;          
-     }
+// let monTableau = new Array('un', 'deux', 'trois');
+// let monTableau = Array('un', 'deux', 'trois');
+let monTableau = ['un', 'deux', 'trois', 'quatre']; // le plus utilisé par les développeurs
 
-     return nombre + somme(nombre - 1);
+// console.log(monTableau[0]); // comment acceder une valeur dans un tableau simple
+console.log(monTableau[monTableau.length - 1]); // comment afficher dinamiquement le dernier résultat.
 
-}
+// let monTableau2D = new Array( 
+//      Array('Mark', 'Jeff', 'Bill'),
+//      Array('Zuckerberg', 'Bezos', 'Gates')
+// ); // Ancienne façon de faire un tableau
 
-console.log(somme(5));
+let monTableau2D = [
+     ['Mark', 'Jeff', 'Bill'],
+     ['Zuckerberg', 'Bezos', 'Gates']
+];
+
+let monTableauAssociatif = { // on associe des noms à des éléments
+     'prenom'  : 'Mark',
+     'nom'     : 'Zuckerberg',
+     'poste'   : 'PDG de Facebook'
+};
+
+console.log(monTableau2D[1][0]); // comment acceder une valeur dans un tableau à plusieurs dimensions
+console.log(monTableauAssociatif['poste']); // comment acceder une valeur dans un tableau associatif
