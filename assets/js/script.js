4ef6d0cab49745b91d8919678b238fec3d970be1
kenjy <kenjyzakary@hotmail.com>
2021-07-28 11:58:33 +0200

    29- Concaténer pour afficher (join)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 
 let monTableau = ['un', 'deux', 'trois', 'quatre']; 
-console.log(monTableau.indexOf('trois'));
+// console.log(monTableau.join(' / '));
 
 let monTableau2D = [
      ['Mark', 'Jeff', 'Bill'],
      ['Zuckerberg', 'Bezos', 'Gates']
 ];
-// console.log(monTableau2D);
+console.log(monTableau2D[0].join(' , '));
+console.log(monTableau2D.join(' / '));
 
-let monTableauAssociatif = { 
+
+let monTableauAssociatif = { // l'option "join" n'est pas disponible pour les tableaux associatif !!
      'prenom'  : 'Mark',
      'nom'     : 'Zuckerberg',
      'poste'   : 'PDG de Facebook'
