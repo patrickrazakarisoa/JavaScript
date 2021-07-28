d89e60caecdc5448d63bbd6690c35dc06e1aa290
kenjy <kenjyzakary@hotmail.com>
2021-07-28 12:25:36 +0200

    30- La fonction ultime pour ajouter, remplacer et supprimer des éléements (splice)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 
 let monTableau = ['un', 'deux', 'trois', 'quatre']; 
-// console.log(monTableau.join(' / '));
+monTableau.splice(2, 0, 'random', 'pie');
+// console.log(monTableau);
 
 let monTableau2D = [
      ['Mark', 'Jeff', 'Bill'],
      ['Zuckerberg', 'Bezos', 'Gates']
 ];
-console.log(monTableau2D[0].join(' , '));
-console.log(monTableau2D.join(' / '));
+monTableau2D.splice(2, 0, ['40', '60', '70']);
+// monTableau2D[0].splice(0, 1)
+console.log(monTableau2D);
 
-
-let monTableauAssociatif = { // l'option "join" n'est pas disponible pour les tableaux associatif !!
+let monTableauAssociatif = { 
      'prenom'  : 'Mark',
      'nom'     : 'Zuckerberg',
      'poste'   : 'PDG de Facebook'
