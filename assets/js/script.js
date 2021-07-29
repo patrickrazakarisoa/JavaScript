fe73bcbeb54994df71b0a29a86fe90f52946a1f3
kenjy <kenjyzakary@hotmail.com>
2021-07-29 20:07:32 +0200

    35- Les fermetures (closures)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
-// let maFonction = function(){
-//      console.log('test');
-// }
+// Les closures permettent de garder en mémoire les variables de fonction parente.
 
-// let maFonction = (parametre) => {
-//      console.log('test');
-// }
+function bonjour(prenom){
+     let resultat = "Bonjour " + prenom; // variable locale
+     let maClosure = () => console.log(resultat);
+     return maClosure;
+}
 
-let maFonction = () => console.log('test');
-
+let maFonction = bonjour('Evan');
 maFonction();
+
+function timer() {
+     let secondes = 0;
+
+     let maClosure = () => {
+          return ++secondes;
+     }
+     return maClosure;
+}
+
+let monTimer = timer();
+console.log(monTimer());
+console.log(monTimer());
+
+let monDeuxiemeTimer = timer();
+console.log(monDeuxiemeTimer());
+
+console.log(monTimer());
+
+
