3cfe433d244f419d48c1f0edc9ff6ad2b763ce14
kenjy <kenjyzakary@hotmail.com>
2021-07-30 15:11:39 +0200

    43- Ajouter un nombre infini d'arguments (REST parameter)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,24 @@
-// Fiche technique sur les objets Set, Map, WeakSet et WeakMap
-// Voici une fiche technique résumant l'ensemble des fonctions liées aux objets Set, Map, WeakSet et WeakMap.
+// function addition(nombreA, nombreB){ // function d'addiction classique
+//      let resultat = nombreA + nombreB;
+//      console.log(resultat);
+// }
 
-//------------------------------------------ L'objet Set
-// Créer un objet Set
-let monObjet = new Set();
-// ou
-let monObjet = new Set(['un', 'deux', 'trois']);
-
-// Ajouter un élément
-monObjet.add('quatre');
-
-// Supprimer un élément
-monObjet.delete('quatre');
-
-// Supprimer tous les éléments
-monObjet.clear();
-
-// Avoir la taille de l'objet (le nombre d'éléments)
-monObjet.size;
-
-// Vérifier si un élément existe (renvoie donc true ou false)
-monObjet.has('un');
-
-// Retourner tous les éléments
-monObjet.values();
-// ou
-monObjet.keys();
+// addition(5, 6);
 
 
-//----------------------------------------- L'objet Map
-// Créer un objet Map
-let monObjet = new Map();
+function addition(...nombres){ // function avec REST parameter
 
-// Ajouter un élément
-monObjet.set('John Doe', {
-    email: 'john@doe.com',
-});
+     let resultat = 0;
+     nombres.forEach(nombre => {
+          resultat += nombre;
+     })
+     console.log(resultat);
 
-// Supprimer un élément
-monObjet.delete('John Doe');
-
-// Supprimer tous les éléments
-monObjet.clear();
-
-// Vérifier si un élément existe (renvoie donc true ou false)
-monObjet.has('John Doe');
-
-// Retourner un élément
-monObjet.get('John Doe');
-
-// Retourner tous les éléments
-monObjet.values();
-// ou
-monObjet.keys();
-
-
-//------------------------------------------------ L'objet WeakSet
-// Créer un objet WeakSet
-let monObjet = new WeakSet();
-// ou
-let monObjet = new WeakSet([objet1, objet2, objet3]);
-
-// Ajouter un élément
-monObjet.add(objet4);
-
-// Supprimer un élément
-monObjet.delete(objet4);
-
-// Avoir la taille de l'objet (le nombre d'éléments)
-monObjet.length();
-
-// Vérifier si un élément existe (renvoie donc true ou false)
-monObjet.has(objet4);
-
-
-//-------------------------------------- L'objet WeakMap
-// Créer un objet WeakMap
-let monObjet = new WeakMap();
-
-// Ajouter un élément
-const objet1 = {
-    nom: 'John Doe',
+     // return nombres.reduce((previous, current) => {
+     //      return previous + current;
+     // });         
 }
- 
-monObjet.set(objet1, {
-    email: 'john@doe.com',
-});
 
-// Supprimer un élément
-monObjet.delete(objet1);
+addition(4, 5, 5, 5, 95, 54, 84, 100);
+// console.log(addition(4, 5, 5, 5, 95, 54, 84, 100));
 
-// Avoir la taille de l'objet (le nombre d'éléments)
-monObjet.length();
-
-// Vérifier si un élément existe (renvoie donc true ou false)
-monObjet.has(objet1);
-
-// Retourner un élément
-monObjet.get(objet1);
