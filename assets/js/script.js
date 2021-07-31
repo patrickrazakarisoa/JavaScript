24f19034ef35aff9fd80d3e8a180de492e25b9d0
kenjy <kenjyzakary@hotmail.com>
2021-07-31 17:23:13 +0200

    45- L'utilisation de  DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,13 @@
-// Exemple 1 : fusionner plusieurs tableaux
-let fruits = ['fraise', 'banane', 'poire'];
-let aliments = ['chocolat', 'sucre', 'lait', ...fruits];
-// let total = aliments.concat(fruits);
-// console.log(aliments);
+let header     = document.getElementsByTagName('header');
+let logo       = document.getElementById('logo');
+let container  = document.getElementsByClassName('container');
+let h1         = document.querySelector('h1');
+let p          = document.querySelectorAll('p');
 
-// Exemple 2 ; décuouper une chaine de caractères en plusierus éléments
-let phrase = "Bonjour !";
-let phraseTableau = [...phrase];
-// console.log(phraseTableau);
+console.log(header);
+console.log(logo);
+console.log(container); 
+console.log(h1); 
+console.log(p); 
 
-// Exemple 2 :  sélectionner un élément , et stocker les autres dans une variable
-let devises = ['dollars', 'euro', 'yen'];
 
-// sans décomposition
-// let premierDevise = devises[0];
-// let autres = [devises[1], devises[2]];
-
-// avec décomposition
-let [ premierDevise, ...autres] = devises;
-
-console.log(premierDevise);
-console.log(autres);
