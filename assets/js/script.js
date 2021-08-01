99d5c15e116ba6f85638a4219c70c93909a38eba
kenjy <kenjyzakary@hotmail.com>
2021-08-01 10:58:03 +0200

    46- Modifier un élément dans le DOM.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-let header     = document.getElementsByTagName('header');
-let logo       = document.getElementById('logo');
-let container  = document.getElementsByClassName('container');
-let h1         = document.querySelector('h1');
-let p          = document.querySelectorAll('p');
+// let h1 = document.querySelector('h1');
+// h1.textContent = "Hello World";
+// h1.innerHTML = "<div style='font-weight: normal; color: steelblue'>Hello World !</div>";
 
-console.log(header);
-console.log(logo);
-console.log(container); 
-console.log(h1); 
-console.log(p); 
+// console.log(h1);
 
+// si on utilise document.getElementsByTagName !!! Au lieu de querySelector
+let h1 = document.getElementsByTagName('h1')[0];
+// h1.textContent = "Hello World";
+h1.innerHTML = "<div style='font-weight: normal; color: steelblue'>Hello World !</div>";
 
+// En une seule ligne
+// let h1 = document.getElementsByTagName('h1')[0].innerHTML = "<div style='font-weight: normal; color: steelblue; text-align: center; padding: 20px;'>Hello World !</div>";
+
+console.log(h1);
