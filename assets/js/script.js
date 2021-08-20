e36cf06f20b3b459e03abf83e0e40296b2690b0a
kenjy <kenjyzakary@hotmail.com>
2021-08-20 15:33:12 +0200

    61- Cféer un objet avec un constructeur personnalisé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,48 @@
-let mark = {
-     prenom: "Mark", // propriété
-     nom: "Zuckerberg",
-     email: "mark@facebook.com",
+// let mark = {
+//      prenom: "Mark", // propriété
+//      nom: "Zuckerberg",
+//      email: "mark@facebook.com",
 
-     // à l'ancienne
-     sePresenter: function(){
-          console.log("Bonjour, je m'appelle " + this.prenom);
+//      // à l'ancienne
+//      sePresenter: function(){
+//           console.log("Bonjour, je m'appelle " + this.prenom);
+//      }
+// }
+
+// On crée une fonction constructeur pour nos utilisateurs
+function Utilisateur(prenom, nom, email) {
+     this.prenom = prenom;
+     this.nom    = nom;
+     this.email  = email;
+
+     this.sePresenter = () => {
+          console.log("Bonjour, je m'appelle " + this.prenom + " " + this.nom + " et vous pouvez me contacter à l'adresse email " + this.email);
      }
 }
 
-// mark.sePresenter();
+// On crée un objet
+var mark = new Utilisateur("Mark", "Zeckerberg", "mark@facebook.com");
+var bill = new Utilisateur("Bill", "Gates", "bill@gatesnotes.com");
 
+// console.log(mark.prenom);
+// console.log(bill.prenom);
 
-// ce qu'il faut faire
-function recevoirLesCoordonnees() {
-     return { latitude: 35, longitude: 139 }
+// bill.sePresenter();
+// console.log(bill);
+
+mark.poste = "PDG de Facebook";
+// console.log(mark);
+
+function Logement(type, annee, placeDeParking, proprietaire) {
+     this.type           = type;
+     this.annee          = annee;
+     this.placeDeParking = placeDeParking;
+     this.proprietaire   = proprietaire;
 }
 
-let coordonnees = recevoirLesCoordonnees();
-console.log(coordonnees.latitude);
-console.log(coordonnees.longitude);
+var appartementA = new Logement('Appartement', 2014, true, mark);// Mark est un objet
+// console.log(appartementA.proprietaire.nom);
+console.log(appartementA);
+
+
+
