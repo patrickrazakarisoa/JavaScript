4db85ace3b063fc5c7ca88e6ef83afc7cf84827c
kenjy <kenjyzakary@hotmail.com>
2021-08-22 21:11:48 +0200

    73- '68- Récupérer la date actuelle'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,5 @@
-// Avant
-// let prenom = "John";
-// let bonjour= "Bonjour " + prenom;
-// console.log(bonjour);
+let dateActuelle = Date();
+console.log(dateActuelle);
 
-// Maintenant
-// let prenom = "John";
-// let bonjour= `Bonjour ${prenom}`;
-// console.log(bonjour);
-
-// Avec les dates
-// let date = new Date().getFullYear();
-// let copyright = `${date} © Portfolio`;
-// console.log(copyright);
-
-// Ou encore plus pratique : des calculs
-let aliments = {fruits: 5, legumes: 1, biscuits: 75}  // objet literal
-let panier   = `Dans votre panier, vous avez ${aliments.fruits + aliments.legumes + aliments.biscuits} articles.`; // Ceca une template string
-console.log(panier); 
+let dateEnSecondes = Date.now();
+console.log(dateEnSecondes);
