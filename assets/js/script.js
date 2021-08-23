d042190887fdd7830ad2d8872dff7f4d7a376a70
kenjy <kenjyzakary@hotmail.com>
2021-08-23 17:18:28 +0200

    74- Utiliser une API : récupérer le prix du bitcoin(XMLhttpRequest)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-let dateActuelle = Date();
-console.log(dateActuelle);
+const url = "https://blockchain.info/ticker";
 
-let dateEnSecondes = Date.now();
-console.log(dateEnSecondes);
+function euroBitCoin() { 
+     // Créer une requête
+     let requete = new XMLHttpRequest(); // Créer un objet
+     requete.open('GET', url); // Premeir paramètre GET / POST, deuxième paramètre : url
+     requete.responseType = 'json'; // Nous attendons du JSON
+     requete.send(); // Nous envoyons notre requête
+
+     // Dès qu'on reçoit une reponse, cette fonction est executée
+     requete.onload = function() {
+          if (requete.readyState === XMLHttpRequest.DONE) {
+               if (requete.status === 200) {
+                    let reponse = requete.response; // on stock la réponse
+                    let prixEnEuros = reponse.EUR.last;
+                    // console.log(prixEnEuros);
+                    document.querySelector('#price_label').textContent = prixEnEuros;             
+               } else {
+                    alert("Un problème est intervenu, merci de revenir plus tard.");
+               }
+          }
+     }
+     console.log('prix actualisé');
+} 
+
+setInterval(euroBitCoin, 1000);
