66fbce25e461b08829e173a008c6e93c80d315a3
kenjy <kenjyzakary@hotmail.com>
2021-08-25 17:09:08 +0200

    79- Les promesses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,40 @@
-// Attention, code puissant
+// Les promesses
+// En cours / Honorée / Rompue
+// Exemple Discord(), discuter en ligne par micro (ou par video)
 
-// Mode synchrone
-let i = 0;
+// const promesse = new Promise((resolve, reject) => {
+//      // Tâches asynchrones
 
-console.log('Fonction commencée');
+//      // Promesse honorée : resolve()
 
-// while(i < 9999999999) {
-//   i++;
-// }
+//      // Promesse rompue : reject()
+// });
 
-console.log('Fonction terminée');
+function chargerScript(script) {
+     return new Promise((resolve, reject) => {
 
+          // Créer un élément
+          let element = document.createElement('script');
+          element.src = script;
+          document.head.append(element);
 
-// Mode asynchrone 
+          // Deux possibilitées : resolve()
+          element.onload = () => resolve('Ficher ' + script + ' a été chargé.');
 
-// function direBonjour(){
-//   console.log('bonjour');
-// }
+          // reject()
+          element.onerror = () => reject(new Error('Opération impossible pour le script ' + script));
+     })
+}
 
-// setTimeout(direBonjour, 3000);
-// console.log('test');
+// const promesse = chargerScript('assets/js/test.js');
+
+// promesse.then(
+//      function(result) {
+//           console.log(result);
+//      },
+//      function(error) {
+//           console.log(error);
+//      }
+// );
+
+chargerScript('assets/js/test.js').catch(console.log)
