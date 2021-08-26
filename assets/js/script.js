222261fb591fd0c9acc13173b9987c7b5ed42dba
kenjy <kenjyzakary@hotmail.com>
2021-08-26 16:42:00 +0200

    80- Async et Await

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-// Les promesses
-// En cours / Honorée / Rompue
-// Exemple Discord(), discuter en ligne par micro (ou par video)
+// // Les promesses
+// // En cours / Honorée / Rompue
+// // Exemple Discord(), discuter en ligne par micro (ou par video)
 
-// const promesse = new Promise((resolve, reject) => {
-//      // Tâches asynchrones
+// // const promesse = new Promise((resolve, reject) => {
+// //      // Tâches asynchrones
 
-//      // Promesse honorée : resolve()
+// //      // Promesse honorée : resolve()
 
-//      // Promesse rompue : reject()
-// });
+// //      // Promesse rompue : reject()
+// // });
 
 function chargerScript(script) {
      return new Promise((resolve, reject) => {
@@ -16,7 +16,7 @@ function chargerScript(script) {
           // Créer un élément
           let element = document.createElement('script');
           element.src = script;
-          document.head.append(element);
+          document.body.append(element);
 
           // Deux possibilitées : resolve()
           element.onload = () => resolve('Ficher ' + script + ' a été chargé.');
@@ -26,15 +26,30 @@ function chargerScript(script) {
      })
 }
 
-// const promesse = chargerScript('assets/js/test.js');
+async function resultat() {
+     try {
+          const scriptA = await chargerScript('assets/js/test.js');
+          console.log(scriptA);
+          const scriptB = await chargerScript('assets/js/autre.js');
+          console.log(scriptB);
+     } catch (error) {
+          console.log(error); 
+          document.body.lastChild.remove();         
+     }
+}
 
-// promesse.then(
-//      function(result) {
-//           console.log(result);
-//      },
-//      function(error) {
-//           console.log(error);
-//      }
-// );
+resultat();
 
-chargerScript('assets/js/test.js').catch(console.log)
+// Async et Await "sucres syntaxiques"
+
+// async function direBonjour() {
+//      const promesse = new Promise((resolve, reject) => {
+//           setTimeout(() => resolve('Promesse tenu !'), 3000);
+
+//      });
+
+//      let resultat = await promesse;
+//      console.log(resultat);
+// }
+
+// direBonjour();
