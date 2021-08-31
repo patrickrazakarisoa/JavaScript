94e4770f24087577cde7ec142a9e4e0cad078b55
kenjy <kenjyzakary@hotmail.com>
2021-08-31 21:28:09 +0200

    86- Ajouter et supprimer un élément

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,17 @@
 // Avec JavaScript
-document.querySelector('h1').textContent = 'Bonjour avec JavaScript';
-// document.querySelector('#monId').className = 'important';
-// document.querySelector('#monId').className = ''; // pour supprimer une classe "impossible"
-// document.querySelector('h1').style.color = 'steelblue';
-
-// document.querySelectorAll('p').textContent = " Ceci est un paragraphe";
-// document.querySelectorAll('p').style.color = "purple"; // impossible
-document.querySelector('.maClass').innerHTML = "<p><b>HTML modifié</b></p>";
+document.querySelector('h1').prepend('Oh : ');
+document.querySelector('h1').append(' !');
+document.querySelectorAll('p')
 
 
 
 
 // Avec JQuery
-$('h1').text("Bonjour avec JQuery"); // p, dpnne le sélecteur CSS
-$('h1').css('color', 'orange');
-$('#monId').addClass('important');
-$('#monId').removeClass('important')
-
-// $('p').text("Ceci est un paragraphe");  // on sélectionne tous les paragraphes
-$('p').css('color', 'purple');
-$('.maClass').html('<p><b>HTML modifié avec Jquery</b></p>');
-
+$('h1').prepend('Oh: ');
+$('h1').append(' !');
+$('h1').before('<div>Hello</div>')
+$('h1').after('<div>Hello</div>')
+$('p').remove();
 
 
 
