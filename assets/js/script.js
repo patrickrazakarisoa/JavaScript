397839497a1c898e2cfad5c2cf7a0972790d3550
kenjy <kenjyzakary@hotmail.com>
2021-08-31 22:16:08 +0200

    89- projet météo ajax plus jolie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,35 @@
-// Avec JavaScript
-// document.querySelector('h1').addEventListener('click', () => {
-//      document.querySelector('h1').style.color= "orange";
-// });
+let apiKey= "a1b6d52affffed92b9c84ab6636fceec";
+let ville = "Paris";
+recevoirTemperature(ville);
 
-// for( let i = 0; i < 2 ; i++) {
-//      document.querySelectorAll('p')[i].addEventListener('click', () => {
-//           document.querySelector('h1').style.color = 'orange';
-//      });
-// }
-
-// Avec JQuery
-// $('h1').click( () => {
-//      $('h1').css('color', 'red');
-// });
-
-$('p').click(() => {
-     $('h1').css('color', 'orange');
-});
-
-$('h1').on('mouseover', () => {
-     $('#monId').addClass('important');
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+     ville =  prompt("Entrer le nom de la ville.");
+     recevoirTemperature(ville);
 })
 
 
+function recevoirTemperature(ville) { 
+     const url= "https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=" + apiKey + "&units=metric";
 
+     let requete = new XMLHttpRequest();
+     requete.open('GET', url);
+     requete.responseType = 'json';
+     requete.send();
+
+     requete.onload = function() {
+          if(requete.readyState === XMLHttpRequest.DONE) {
+               if(requete.status === 200) {
+                    let response = requete.response;
+                    let responseTemp  = response.main.temp;
+                    let responseVille = response.name;
+
+                    document.querySelector('#ville').textContent = responseVille;
+                    document.querySelector('#temperature_label').textContent = responseTemp;
+               } else {
+                    alert("Un problème est intervenu, merci de revenir plus tard.");
+               }
+          }
+     }
+}
 
